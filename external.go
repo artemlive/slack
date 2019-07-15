6c2c5b60fb24fbf7f13ae2dde0a36a941703c4e2
Artem Miroshnichenko <a.miroshnichenko@epom.com>
2019-07-15 16:07:47 +0300

    ParseExternalContent request parser added

---
+++ external.go
@@ -0,0 +1,50 @@
+package slack
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/artemlive/slack/slackevents"
+)
+
+type ExternalRequest struct {
+	Name       string `json:"name"`
+	Value      string `json:"value"`
+	CallbackID string `json:"callback_id"`
+	Type       string `json:"type"`
+	Team       struct {
+		ID     string `json:"id"`
+		Domain string `json:"domain"`
+	} `json:"team"`
+	Channel struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"channel"`
+	User struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"user"`
+	ActionTs     string `json:"action_ts"`
+	MessageTs    string `json:"message_ts"`
+	AttachmentID string `json:"attachment_id"`
+	Token        string `json:"token"`
+}
+
+
+func ParseExternalContent(body string, opts ...slackevents.Option) (ExternalRequest, error) {
+	byteString := []byte(body)
+	action := ExternalRequest{}
+	err := json.Unmarshal(byteString, &action)
+	if err != nil {
+		return ExternalRequest{}, errors.New("ExternalRequest unmarshalling failed")
+	}
+	cfg := &slackevents.Config{}
+	cfg.VerificationToken = action.Token
+	for _, opt := range opts {
+		opt(cfg)
+	}
+	if !cfg.TokenVerified {
+		return ExternalRequest{}, errors.New("invalid verification token")
+	} else {
+		return action, nil
+	}
+}
